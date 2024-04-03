dd8b05816f2ecb35f33e723a554acf0163f7f389
Ricardo Castro <soyricardodev@proton.me>
2024-04-02 22:41:09 -0400

    run formatter and add .gitignore to scaffold

--- index.ts
+++ index.ts
@@ -4,17 +4,18 @@ await $`echo "Lets do some stuff!"`;
 
 await $.cwd("/home/ricardo/sandbox/");
 
-const projectFolderName = prompt("Enter a name for the proyect: ")
-const defaultFolderName = "my-python-project"
+const projectFolderName = prompt("Enter a name for the proyect: ");
+const defaultFolderName = "my-python-project";
 
-const projectName = projectFolderName !== null ?
-  projectFolderName.trim().toLowerCase().replace(/\s/g, "-") :
-  defaultFolderName
+const projectName =
+	projectFolderName !== null
+		? projectFolderName.trim().toLowerCase().replace(/\s/g, "-")
+		: defaultFolderName;
 
-await $`echo "Creating folder ${projectName}"`
-await $`mkdir ${projectName}`
+await $`echo "Creating folder ${projectName}"`;
+await $`mkdir ${projectName}`;
 
-const pyprojectToml = `[project]
+const pyprojectDotToml = `[project]
 name = "${projectName}"
 requires-python = ">=3.8"
 dependencies = ["ruff"]
@@ -49,26 +50,33 @@ select = [
 [tool.ruff.format]
 indent-style = "tab"
 docstring-code-format = true
-`
-const requirementsTxt = "ruff"
+`;
+const requirementsDotTxt = "ruff";
+
+const dotGitIgnore = `.venv
+venv
+node_modules
+__pycache__
+.ruff_cache
+`;
 
 const vscodeSettings = `{
   "[python]": {
     "editor.formatOnSave": true,
     "editor.defaultFormatter": "charliermarsh.ruff"
   }
-}`
+}`;
 
 const vscodeExtensions = `{
   "recommendations": ["charliermarsh.ruff", "ms-python.vscode-pylance", "ms-python.python", "ms-python.debugpy", "tamasfe.even-better-toml"],
   "unwantedRecommendations": ["ms-python.black-formatter", "ms-python.pylint"]
-}`
+}`;
 
-await $`cd ${projectName} && uv venv && echo "${pyprojectToml}" > pyproject.toml && echo "${requirementsTxt}" > requirements.txt && mkdir .vscode && echo "${vscodeSettings}" > .vscode/settings.json && echo "${vscodeExtensions}" > .vscode/extensions.json`
+await $`cd ${projectName} && uv venv && echo "${pyprojectDotToml}" > pyproject.toml && echo "${requirementsDotTxt}" > requirements.txt && mkdir .vscode && echo "${vscodeSettings}" > .vscode/settings.json && echo "${vscodeExtensions}" > .vscode/extensions.json && echo "${dotGitIgnore}" > .gitignore`;
 
-console.log("Now activate the venv with:")
-console.log("  source venv/bin/activate")
-console.log("And install the dependencies with:")
-console.log("  uv pip install -r requirements.txt") 
+console.log("Now activate the venv with:");
+console.log("  source venv/bin/activate");
+console.log("And install the dependencies with:");
+console.log("  uv pip install -r requirements.txt");
 
-console.log("Done!")
+console.log("Done!");
